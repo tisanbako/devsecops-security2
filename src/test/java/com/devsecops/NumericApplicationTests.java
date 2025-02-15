0f639f651144511a433006118f21b2016c676f68
tisanbako <bakotisan@gmail.com>
2025-02-15 17:55:39 -0500

    adding files

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -51,7 +51,7 @@ public class NumericApplicationTests {
 @Test
     public void welcomeMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Kubernetes DevSecOps"));
+                .andExpect(content().string("Kubernetes For DevSecOps"));
     }
     
 
