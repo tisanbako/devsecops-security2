afdc142106fd88656198e6415b4a669dafcd91e6
tisanbako <bakotisan@gmail.com>
2025-02-15 17:53:13 -0500

    adding files

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes For DevSecOps";
 		}
 
 		@GetMapping("/compare/{value}")
