07bb1ffd03177b5c447b18f96a56c2c4c038ba81
tisanbako <bakotisan@gmail.com>
2025-02-15 17:38:20 -0500

    addng no tisan

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Tisan Kubernetes DevSecOps";
+			return "Kubernetes DevSecOps";
 		}
 
 		@GetMapping("/compare/{value}")
